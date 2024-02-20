a2091597adf29f491c358b7375f6b96491f372a6
Vanya Slesarev <119366827+shashlyk55@users.noreply.github.com>
2024-02-20 22:22:10 +0300

    Add files via upload

---
+++ lab01/lab01/lab01/Program.cpp
@@ -0,0 +1,57 @@
+#include "Auxil.h"                            // вспомогательные функции 
+#include <iostream>
+#include <ctime>
+#include <locale>
+#include <tchar.h>
+
+#define  CYCLE	1000000                       // количество циклов  
+
+int rec(int n) {
+	if (n == 1) 
+		return 1;
+	if (n == 0)
+		return 0;
+	else 
+		return rec(n-1) + rec(n - 2);
+}
+
+using namespace std;
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	double  av1 = 0, av2 = 0;
+	clock_t  t1 = 0, t2 = 0;
+
+	setlocale(LC_ALL, "rus");
+
+	auxil::start();                          // старт генерации 
+	t1 = clock();                            // фиксация времени 
+	for (int i = 0; i < CYCLE; i++)
+	{
+		av1 += (double)auxil::iget(-100, 100); // сумма случайных чисел 
+		av2 += auxil::dget(-100, 100);         // сумма случайных чисел 
+	}
+	t2 = clock();                            // фиксация времени 
+
+
+	std::cout << std::endl << "количество циклов:         " << CYCLE;
+	std::cout << std::endl << "среднее значение (int):    " << av1 / CYCLE;
+	std::cout << std::endl << "среднее значение (double): " << av2 / CYCLE;
+	std::cout << std::endl << "продолжительность (у.е):   " << (t2 - t1);
+	std::cout << std::endl << "                  (сек):   "
+		<< ((double)(t2 - t1)) / ((double)CLOCKS_PER_SEC);
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	t1 = clock();
+	cout << rec(40) << endl;
+	t2 = clock();
+
+	std::cout << std::endl << "продолжительность (у.е):   " << (t2 - t1);
+	std::cout << std::endl << "                  (сек):   " << ((double)(t2 - t1)) / ((double)CLOCKS_PER_SEC) << endl;
+	
+
+	system("pause");
+
+	return 0;
+}
